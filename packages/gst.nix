cc37a2c8b16f5dd5c63bb6b382625a4f70d9b3bb
Otto J. Hermann <ojhermann@gmail.com>
2026-04-08 20:33:20 -0400

    fix: correct Nix string delimiters in gst.nix

--- packages/gst.nix
+++ packages/gst.nix
@@ -6,11 +6,11 @@ pkgs.writeShellApplication {
     pkgs.git
     pkgs.tree
   ];
-  text = '''
+  text = ''
     if git rev-parse --git-dir >/dev/null 2>&1; then
       git status -sb; { git ls-files; git ls-files --others --exclude-standard; } | sort -u | tree -C --fromfile .
     else
       tree -aC
     fi
-  ''';
+  '';
 }
